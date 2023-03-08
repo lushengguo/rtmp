c2b964066a85a5213fb103a6ef887953d1f5220c
KAGURA <53362727+lushengguo@users.noreply.github.com>
2023-03-08 08:33:42 +0000

    init

---
+++ rtmp.h
@@ -0,0 +1,17 @@
+#ifndef RTMP_HPP
+#define RTMP_HPP
+
+struct BasicHeader{};
+
+struct MessageHeader{};
+
+struct ExtenedTimestamp{};
+
+struct ChunkData{};
+
+struct ChunkHeader{};
+
+struct __attribute__((__packed__))  RTMP_pack{
+};
+
+#endif
